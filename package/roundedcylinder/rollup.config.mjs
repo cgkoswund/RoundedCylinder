366643784dd78f76dc9813ae7b18afc9c227cc00
cgkoswund <tnyaffram@gmail.com>
2025-01-01 15:23:34 +0000

    set up rollup build config

--- package/roundedcylinder/rollup.config.mjs
+++ package/roundedcylinder/rollup.config.mjs
@@ -1,9 +1,22 @@
+import babel from "@rollup/plugin-babel";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+
 export default {
-  input: "src/index.js",
+  input: "src/index.jsx",
   output: [
     {
       file: "dist/index.esm.js", // Export ESM
       format: "esm", // Ensure ESM output
     },
   ],
+  plugins: [
+    nodeResolve(), // Resolve node_modules
+    commonjs(), // Convert CommonJS to ES Modules
+    babel({
+      babelHelpers: "bundled",
+      extensions: [".js", ".jsx"], // Include JSX files
+    }),
+  ],
+  external: ["react", "react-dom", "three"], // Peer dependencies
 };
